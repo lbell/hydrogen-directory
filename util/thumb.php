b84012a91e417363d81e2dedff2dd5912f5bcf1d
lbell <1029014+lbell@users.noreply.github.com>
2021-06-19 07:50:37 -0600

    image cleanup

--- util/thumb.php
+++ util/thumb.php
@@ -3,6 +3,6 @@ function hydir_thumb($id, $size = "thumb") {
 	if (has_post_thumbnail($id)) {
 		return get_the_post_thumbnail($id, $size, array('class' => ''));
 	} else {
-		return '<img class="" src="' . HYDIR_URL . 'public/img/avatar_blank.png" alt="" />';
+		return '<img class="" src="' . HYDIR_URL . 'public/img/avatar_blank.jpg" alt="" />';
 	}
 }
